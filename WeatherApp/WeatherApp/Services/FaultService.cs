573e1a508a0ffc62dcc3e14387280ab702e6d207
KOHb <shebalovdenis@gmail.com>
2020-10-16 18:19:48 +0300

    additional refactoring

--- WeatherApp/WeatherApp/Services/FaultService.cs
+++ WeatherApp/WeatherApp/Services/FaultService.cs
@@ -52,47 +52,5 @@ namespace WeatherApp.Services
 
             return chartModel;
         }
-
-        public Stream GetFileWithFaults(ChartModel faults)
-        {
-            var stream = new MemoryStream();
-
-            using (var workbook = new XLWorkbook())
-            {
-                var ws = workbook.Worksheets.Add("Sample Sheet");
-
-                ws.Column(2).Width = 20;
-                var k = 0;
-
-                for (var i = 0; i < faults.ChartData.Count; i++)
-                {
-                    ws.Cell("C4").Value = $"Погрешности измерений на {DateTime.Now}";
-                    ws.Range("C4:G4").Row(1).Merge();
-
-                    ws.Cell($"B{5 + k}").Value = $"Интервал - {faults.ChartData[i].interval}";
-                    ws.Cell($"B{6 + k}").Value = "Интервал";
-                    ws.Cell($"B{7 + k}").Value = "Погрешность";
-
-                    for (var j = 0; j < faults.ChartData[i].Item1.Count; j++)
-                    {
-                        ws.Column(3 + j).Width = 15;
-                        ws.Cell(6 + k, 3 + j).Value = faults.ChartData[i].Item1[j].date;
-                    }
-
-                    for (var j = 0; j < faults.ChartData[i].Item1.Count; j++)
-                    {
-                        ws.Cell(7 + k, 3 + j).Value = faults.ChartData[i].Item1[j].temp;
-                    }
-
-                    k += 4;
-
-                }
-
-                workbook.SaveAs(stream);
-                stream.Position = 0;
-            }
-
-            return stream;
-        }
     }
 }
--- WeatherApp/WeatherApp/Services/Interfaces/IFaultService.cs
+++ WeatherApp/WeatherApp/Services/Interfaces/IFaultService.cs
@@ -12,6 +12,5 @@ namespace WeatherApp.Services.Interfaces
     public interface IFaultService
     {
         ChartModel CalculateFaults(IEnumerable<ForecastModel> historicalForecast, IEnumerable<IEnumerable<ForecastModel>> intervalForecast);
-        Stream GetFileWithFaults(ChartModel faults);
     }
 }
